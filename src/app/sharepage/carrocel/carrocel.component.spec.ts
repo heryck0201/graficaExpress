5adc0efa02b6b399ab5e75523c08d3ffd21e1d56
heryck0201 <herycktarso@outlook.com>
2025-10-16 09:56:43 -0300

    initial commit

---
+++ src/app/sharepage/carrocel/carrocel.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CarrocelComponent } from './carrocel.component';
+
+describe('CarrocelComponent', () => {
+  let component: CarrocelComponent;
+  let fixture: ComponentFixture<CarrocelComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CarrocelComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CarrocelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/sharepage/carrocel/carrocel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carrocel',
+  templateUrl: './carrocel.component.html',
+  styleUrls: ['./carrocel.component.css']
+})
+export class CarrocelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
