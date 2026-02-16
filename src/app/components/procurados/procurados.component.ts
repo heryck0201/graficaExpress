f073056d71f8d9f49ab8d54a701a401f46f98e4f
heryck0201 <herycktarso@outlook.com>
2026-02-16 14:06:54 -0300

    conclusão do sobre

--- src/app/components/procurados/procurados.component.ts
+++ src/app/components/procurados/procurados.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 interface Trabalho {
+  id: number;
   titulo: string;
   descricao: string;
   img: string;
 }
+
 
 @Component({
   selector: 'app-procurados',
@@ -15,31 +19,42 @@ export class ProcuradosComponent implements OnInit {
 
   trabalhos: Trabalho[] = [];
 
-  constructor() { }
+ constructor(private router: Router) { }
+
 
   ngOnInit(): void {
     this.trabalhos = [
-      {
-        titulo: 'Panfletos',
-        descricao: 'Materiais gráficos com alta qualidade, rapidez e excelente acabamento.',
-        img: 'assets/trabalhos/trabalho1.jpg'
-      },
-      {
-        titulo: 'Cartões de visita',
-        descricao: 'Canecas, camisas, copos, brindes e produtos exclusivos para sua marca.',
-        img: 'assets/trabalhos/trabalho2.jpg'
-      },
-      {
-        titulo: 'Calendários e Agendas',
-        descricao: 'Produção personalizada para empresas, eventos e ações promocionais.',
-        img: 'assets/trabalhos/trabalho3.jpg'
-      },
-      {
-        titulo: 'Banner e adesivos',
-        descricao: 'Banners, faixas e materiais visuais para destacar seu negócio.',
-        img: 'assets/trabalhos/trabalho4.jpg'
-      }
-    ];
+  {
+    id: 1,
+    titulo: 'Panfletos',
+    descricao: 'Materiais gráficos com alta qualidade, rapidez e excelente acabamento.',
+    img: 'assets/imgPanfleto/panfleto1.jpg'
+  },
+  {
+    id: 4,
+    titulo: 'Cartões de visita',
+    descricao: 'Canecas, camisas, copos, brindes e produtos exclusivos para sua marca.',
+    img: 'assets/imgCartoesDiversos/cartaoVisita1.jpg'
+  },
+  {
+    id: 5,
+    titulo: 'Calendários e Agendas',
+    descricao: 'Produção personalizada para empresas, eventos e ações promocionais.',
+    img: 'assets/imgCardapiosCartaz/cartaz1.jpg'
+  },
+  {
+    id: 6,
+    titulo: 'Canecas,Copos e Garrafas',
+    descricao: 'Banners, faixas e materiais visuais para destacar seu negócio.',
+    img: 'assets/imgCanecasCoposGarrafas/garrafa1.jpg'
   }
+];
+
+  }
+
+goToProduct(id: number) {
+  this.router.navigate(['/produto', id]);
+}
+
 
 }
